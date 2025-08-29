1e5bfe15d69b30d660493e42576ea4d335e16285
Lubomyr Soroka <113640042+cantThinkOfAUserNam@users.noreply.github.com>
2025-08-29 16:56:26 -0600

    Added GitHub Actions script

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286aa981ff4e7e6770af6496a5c919d58124abc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2365be383daed64ccdf52f32b6ee24dc56976b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
