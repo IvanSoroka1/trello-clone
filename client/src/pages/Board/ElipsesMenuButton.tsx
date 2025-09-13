276cf0c18d6b9f1ce1e987faf8b7adeba6b0a912
Lubomyr Soroka <113640042+cantThinkOfAUserNam@users.noreply.github.com>
2025-09-13 11:16:04 -0600

    fixed issue with board menu layering order. Fixed the delete menu dimming problem.

--- client/src/pages/Board/ElipsesMenuButton.tsx
+++ client/src/pages/Board/ElipsesMenuButton.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
-import {fetchWithRefresh} from "../../Refresh.tsx";
+import { fetchWithRefresh } from "../../Refresh.tsx";
+import { createPortal } from "react-dom";
 
 
 export default function ElipsesMenuButton({ id }: { id: number }) {
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false)
     const [confirmDeleteMenu, setConfirmDeleteMenu] = useState(false);
+    const [position, setPosition] = useState<{ top: number; right: number }>({ top: 0, right: 0 });
     useEffect(() => {
         const closeMenu = () => {
             setShowMenu(false);
@@ -42,12 +44,22 @@ export default function ElipsesMenuButton({ id }: { id: number }) {
 
     return (
         <div>
-            <button onClick={(e) => { e.stopPropagation(); setShowMenu(!showMenu) }} className="rounded p-1">
+            <button onClick={(e) => { e.stopPropagation(); setShowMenu(!showMenu); const rect = e.currentTarget.getBoundingClientRect();
+             setPosition({
+                top: rect.bottom, // below button
+                right: window.innerWidth - rect.right,  // aligned to right
+            }); }} className="rounded p-1">
                 <FiMoreHorizontal size={20} />
             </button>
             {
-                showMenu &&
-                <div className="w-80 absolute top-full right-0 shadow-lg rounded-lg bg-white w-32">
+            showMenu &&
+                createPortal(
+                <div className="z-50 w-80 shadow-lg rounded-lg bg-white w-32"
+                style={{
+                            top: position.top,
+                            right: position.right,
+                            position: "fixed",
+                }}>
                     <div className="flex justify-center font-bold">
                         Board Options
                     </div>
@@ -57,23 +69,27 @@ export default function ElipsesMenuButton({ id }: { id: number }) {
                         </button>
                     </div>
                 </div>
+                , document.body)
             }
 
             {
                 confirmDeleteMenu &&
-                <div className="z-5 fixed inset-0 bg-[#00000080]  flex justify-center items-center">
-                    <div className="relative w-1/3 h-1/2 text-4xl bg-white rounded text-center p-6">
-                        Are you sure you want to delete this board? This action cannot be undone.
+                createPortal(
+                    <div className="z-50 fixed inset-0 bg-[#00000080]  flex justify-center items-center">
+                        <div className="relative w-1/3 h-1/2 text-4xl bg-white rounded text-center p-6">
+                            Are you sure you want to delete this board? This action cannot be undone.
 
-                        <div onClick={deleteBoard} className="w-1/3 absolute bottom-6 left-12 bg-green-500 text-white hover:bg-green-700">
-                            Yes
+                            <div onClick={deleteBoard} className="w-1/3 absolute bottom-6 left-12 bg-green-500 text-white hover:bg-green-700">
+                                Yes
+                            </div>
+                            <div onClick={() => setConfirmDeleteMenu(false)} className="w-1/3 absolute bottom-6 right-12 bg-red-500 text-white hover:bg-red-700">
+                                No
+                            </div>
                         </div>
-                        <div onClick={() => setConfirmDeleteMenu(false)}className="w-1/3 absolute bottom-6 right-12 bg-red-500 text-white hover:bg-red-700">
-                            No
-                        </div>
+
                     </div>
-
-                </div>
+                     , document.body
+                 )
             }
         </div>
 
